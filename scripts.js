b2d0ab2d472338c9d619c3b01145c90681eda59f
Jcbgid <jgid261@hotmail.com>
2016-10-07 20:16:44 -0500

    JSON Update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-Jcbgid/master/logan.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
     $.each(data.names, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+      $("#nicknames").append("<li>" + value.theme + "</li>");
     });
   });
 });
