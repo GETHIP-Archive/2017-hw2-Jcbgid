f10a9f77bf18a03050774da6bc52745e00f1aaa6
Jcbgid <jgid261@hotmail.com>
2016-10-07 21:21:16 -0500

    JSON Update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.getJSON("https://dl.dropboxusercontent.com/u/72474818/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-Jcbgid/master/logan.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
